046e36466050286f24114fd5682feb5f9ccc1ece
Akhmad Mustofa Solikin <91455464+mustofa08@users.noreply.github.com>
2023-10-22 21:57:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,167 +1,210 @@
-const word = document.getElementById("word");
-const incorrect = document.getElementById("incorrect");
-const incorrectLettersEl = document.querySelector("#incorrect p");
-const backdrop = document.getElementById("backdrop");
-const finalMsg = document.getElementById("final-msg");
-const msgInfo = document.getElementById("msg-info");
-const playBtn = document.getElementById("play");
-const indication = document.getElementById("indication");
-const bodyParts = document.getElementsByClassName("body-part");
-
-// List of words
-const wordList = [
-  { word: "amerika", hint: "Negri Paman Sam" },
-  { word: "indonesia", hint: "Zamrud Katulistiwa" },
-  { word: "malaysia", hint: "Negri Jiran" },
-  { word: "laos", hint: "Tanah Terkunci" },
-  { word: "thailand", hint: "Negri Gajah Putih" },
-  { word: "brazil", hint: "Negri Samba" },
-  { word: "filiphina", hint: "Negri Lumpung Padi" },
-  { word: "irak", hint: "Negri 1001 Malam" },
-  { word: "myanmar", hint: "Tanah Emas" },
-  { word: "jepang", hint: "Negri Matahari Terbit" },
-  { word: "china", hint: "Negri tirai bambu" },
-];
-
-// that is selected to play
-let selectedWord = null;
-// Stores the count of no.of incorrectly typed letters
-let incorrectCount = 0;
-// Correct letters typed by the player
-const correctLetters = [];
-// Incorrect letters typed by the player
-const incorrectLetters = [];
-
-// Batasan jumlah kesalahan menebak
-const maxIncorrectGuesses = 6;
-
-// Select a word randomly from wordList and initialize in the DOM
-function initializeWord() {
-  selectedWord = getRandomWord();
-  const noOfLetters = selectedWord.word.length;
-  for (let i = 0; i < noOfLetters; i++) {
-    const listItem = document.createElement("li");
-    listItem.classList.add("letter");
-    word.append(listItem);
+  // Fungsi untuk menampilkan halaman registrasi
+  function showRegistrationPage() {
+    document.getElementById("registration-page").style.display = "block";
+    document.getElementById("login-page").style.display = "none";
+    document.getElementById("game-page").style.display = "none";
   }
 
-  // Tampilkan hint kata kepada pemain
-  document.getElementById("categoryHint").textContent = `Hint: ${selectedWord.hint}`;
-}
+  // Fungsi untuk menampilkan halaman login
+  function showLoginPage() {
+    document.getElementById("registration-page").style.display = "none";
+    document.getElementById("login-page").style.display = "block";
+    document.getElementById("game-page").style.display = "none";
+  }
 
-// Displays an indication sliding from the bottom
-function displayIndication() {
-  indication.classList.add("visible");
+  // Fungsi untuk menampilkan halaman permainan setelah login berhasil
+  function showGamePage() {
+    document.getElementById("registration-page").style.display = "none";
+    document.getElementById("login-page").style.display = "none";
+    document.getElementById("game-page").style.display = "block";
+    initializeWord(); // Panggil inisialisasi permainan di sini saat permainan dimulai.
+  }
 
-  setTimeout(() => {
-    indication.classList.remove("visible");
-  }, 2400);
-}
+  // Fungsi untuk menangani proses registrasi
+  function handleRegistration(event) {
+    event.preventDefault();
+    showLoginPage();
+  }
 
-// Update the figure when incorrect letters typed
-function updateFigure() {
-  try {
-    bodyParts[incorrectCount].style.display = "block";
-    incorrectCount++;
-  } catch (error) {}
-}
+  // Fungsi untuk menangani proses login
+  function handleLogin(event) {
+    event.preventDefault();
+    showGamePage(); // Panggil fungsi untuk memulai permainan saat login berhasil.
+  }
 
-// When player wins
-function successState() {
-  setTimeout(() => {
-    backdrop.classList.add("visible");
-    finalMsg.classList.add("visible");
-    msgInfo.textContent = "Selamar Tebakanmu Benar";
-  }, 400);
-}
+  // Panggil fungsi showRegistrationPage() saat halaman dimuat
+  showRegistrationPage();
 
-// When player loses
-function failureState() {
-  setTimeout(() => {
-    backdrop.classList.add("visible");
-    finalMsg.classList.add("visible");
-    msgInfo.textContent = `Kamu Kalah. Kata yang benar adalah "${selectedWord.word}"`;
-  }, 400);
-}
+  // Event listener untuk form registrasi
+  document.getElementById("registration-form").addEventListener("submit", handleRegistration);
 
-// Check if typed key is part of the selected word and update in the DOM if required
-function check(ev) {
-  const letterElements = document.querySelectorAll(".word .letter");
-  const character = ev.key;
+  // Event listener untuk form login
+  document.getElementById("login-form").addEventListener("submit", handleLogin);
 
-  // Handle keyboard events
-  if (!backdrop.classList.contains("visible") && !indication.classList.contains("visible") && ev.keyCode >= 65 && ev.keyCode <= 90 && incorrectCount < maxIncorrectGuesses) {
-    if (selectedWord.word.includes(character)) {
-      if (correctLetters.includes(character)) {
-        displayIndication();
-      } else {
-        correctLetters.push(character);
-        const indexes = [];
-        [...selectedWord.word].forEach((value, index) => {
-          if (value === character) {
-            indexes.push(index);
-          }
-        });
-        indexes.forEach((value) => {
-          letterElements[value].textContent = character;
-        });
-      }
-    } else {
-      if (incorrectLetters.includes(character)) {
-        displayIndication();
-      } else {
-        incorrectLetters.push(character);
-        if (!incorrect.classList.contains("visible")) {
-          incorrect.classList.add("visible");
+  // ... Hangman ...
+
+  // ...Hangman ...
+  const word = document.getElementById("word");
+  const incorrect = document.getElementById("incorrect");
+  const incorrectLettersEl = document.querySelector("#incorrect p");
+  const backdrop = document.getElementById("backdrop");
+  const finalMsg = document.getElementById("final-msg");
+  const msgInfo = document.getElementById("msg-info");
+  const playBtn = document.getElementById("play");
+  const indication = document.getElementById("indication");
+  const bodyParts = document.getElementsByClassName("body-part");
+
+  // List of words
+  const wordList = [
+    { word: "amerika", hint: "Negri Paman Sam" },
+    { word: "indonesia", hint: "Zamrud Katulistiwa" },
+    { word: "malaysia", hint: "Negri Jiran" },
+    { word: "laos", hint: "Tanah Terkunci" },
+    { word: "thailand", hint: "Negri Gajah Putih" },
+    { word: "brazil", hint: "Negri Samba" },
+    { word: "filiphina", hint: "Negri Lumpung Padi" },
+    { word: "irak", hint: "Negri 1001 Malam" },
+    { word: "myanmar", hint: "Tanah Emas" },
+    { word: "jepang", hint: "Negri Matahari Terbit" },
+    { word: "china", hint: "Negri tirai bambu" },
+  ];
+
+  // that is selected to play
+  let selectedWord = null;
+  // Stores the count of no.of incorrectly typed letters
+  let incorrectCount = 0;
+  // Correct letters typed by the player
+  const correctLetters = [];
+  // Incorrect letters typed by the player
+  const incorrectLetters = [];
+
+  // Batasan jumlah kesalahan menebak
+  const maxIncorrectGuesses = 6;
+
+  // Select a word randomly from wordList and initialize in the DOM
+  function initializeWord() {
+    selectedWord = getRandomWord();
+    const noOfLetters = selectedWord.word.length;
+    for (let i = 0; i < noOfLetters; i++) {
+      const listItem = document.createElement("li");
+      listItem.classList.add("letter");
+      word.append(listItem);
+    }
+
+    // Tampilkan hint kata kepada pemain
+    document.getElementById("categoryHint").textContent = `Hint: ${selectedWord.hint}`;
+  }
+
+  // Displays an indication sliding from the bottom
+  function displayIndication() {
+    indication.classList.add("visible");
+
+    setTimeout(() => {
+      indication.classList.remove("visible");
+    }, 2400);
+  }
+
+  // Update the figure when incorrect letters typed
+  function updateFigure() {
+    try {
+      bodyParts[incorrectCount].style.display = "block";
+      incorrectCount++;
+    } catch (error) {}
+  }
+
+  // When player wins
+  function successState() {
+    setTimeout(() => {
+      backdrop.classList.add("visible");
+      finalMsg.classList.add("visible");
+      msgInfo.textContent = "Selamar Tebakanmu Benar";
+    }, 400);
+  }
+
+  // When player loses
+  function failureState() {
+    setTimeout(() => {
+      backdrop.classList.add("visible");
+      finalMsg.classList.add("visible");
+      msgInfo.textContent = `Kamu Kalah. Kata yang benar adalah "${selectedWord.word}"`;
+    }, 400);
+  }
+
+  // Check if typed key is part of the selected word and update in the DOM if required
+  function check(ev) {
+    const letterElements = document.querySelectorAll(".word .letter");
+    const character = ev.key;
+
+    // Handle keyboard events
+    if (!backdrop.classList.contains("visible") && !indication.classList.contains("visible") && ev.keyCode >= 65 && ev.keyCode <= 90 && incorrectCount < maxIncorrectGuesses) {
+      if (selectedWord.word.includes(character)) {
+        if (correctLetters.includes(character)) {
+          displayIndication();
+        } else {
+          correctLetters.push(character);
+          const indexes = [];
+          [...selectedWord.word].forEach((value, index) => {
+            if (value === character) {
+              indexes.push(index);
+            }
+          });
+          indexes.forEach((value) => {
+            letterElements[value].textContent = character;
+          });
         }
-        incorrectLettersEl.textContent = `${incorrectLetters.join(", ")}`;
-        updateFigure();
+      } else {
+        if (incorrectLetters.includes(character)) {
+          displayIndication();
+        } else {
+          incorrectLetters.push(character);
+          if (!incorrect.classList.contains("visible")) {
+            incorrect.classList.add("visible");
+          }
+          incorrectLettersEl.textContent = `${incorrectLetters.join(", ")}`;
+          updateFigure();
+        }
       }
+    }
+
+    // Create a word from all letter items
+    let formedWord = "";
+    letterElements.forEach((value) => {
+      formedWord += value.textContent;
+    });
+
+    // Check if created word is correct
+    if (formedWord === selectedWord.word) {
+      successState();
+    }
+
+    // Check if man was hung
+    if (incorrectCount >= maxIncorrectGuesses) {
+      failureState();
     }
   }
 
-  // Create a word from all letter items
-  let formedWord = "";
-  letterElements.forEach((value) => {
-    formedWord += value.textContent;
-  });
-
-  // Check if created word is correct
-  if (formedWord === selectedWord.word) {
-    successState();
+  // Reset all variables and start a new game
+  function startNewGame() {
+    selectedWord = null;
+    incorrectCount = 0;
+    correctLetters.splice(0);
+    incorrectLetters.splice(0);
+    word.innerHTML = "";
+    Array.from(bodyParts).forEach((value) => {
+      value.style.display = "none";
+    });
+    incorrect.classList.remove("visible");
+    backdrop.classList.remove("visible");
+    finalMsg.classList.remove("visible");
+    initializeWord();
   }
 
-  // Check if man was hung
-  if (incorrectCount >= maxIncorrectGuesses) {
-    failureState();
+  // Event Listeners
+  window.addEventListener("keyup", check);
+  playBtn.addEventListener("click", startNewGame);
+
+  // Fungsi untuk mengambil kata acak beserta hintnya
+  function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * wordList.length);
+    return wordList[randomIndex];
   }
-}
-
-// Reset all variables and start a new game
-function startNewGame() {
-  selectedWord = null;
-  incorrectCount = 0;
-  correctLetters.splice(0);
-  incorrectLetters.splice(0);
-  word.innerHTML = "";
-  Array.from(bodyParts).forEach((value) => {
-    value.style.display = "none";
-  });
-  incorrect.classList.remove("visible");
-  backdrop.classList.remove("visible");
-  finalMsg.classList.remove("visible");
-  initializeWord();
-}
-
-// Start the game
-initializeWord();
-
-// Event Listeners
-window.addEventListener("keyup", check);
-playBtn.addEventListener("click", startNewGame);
-
-// Fungsi untuk mengambil kata acak beserta hintnya
-function getRandomWord() {
-  const randomIndex = Math.floor(Math.random() * wordList.length);
-  return wordList[randomIndex];
-}
